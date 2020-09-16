73f37dbbf9de31538ae0a3f97815dcbff5f79736
Juliane Reinhardt <jreinhardt@lbl.gov>
2020-09-16 00:18:40 -0700

    messing with the test

--- xicam/tests/test_examples.py
+++ xicam/tests/test_examples.py
@@ -9,7 +9,7 @@ from scipy.ndimage.interpolation import shift
 
 from pystxmtools.corrections.register import register_frames_stack
 
-@fixture
+# @fixture
 def test_stack():
     image = data.camera()
     n_frames = 10
@@ -17,18 +17,32 @@ def test_stack():
     im_shift_stack = []
     for n in range(n_frames):
         shifts = (round(random.uniform(1, 50), 2), round(random.uniform(1, 50), 2))
-        el = [shift(image, shifts), 0]
+        el = [shift(image, shifts), shifts]
         im_shift_stack.append(el)
     return np.asarray(im_shift_stack)
 
 
-def test_register_frame_stack(test_stack):
+def test_register_frame_stack():
     "Test something in register_frame_stack "
-    aligned_frames, calc_shifts = register_frames_stack(frames=test_stack[:,0])
+    image = data.camera()
+    n_frames = 10
+    # shifts = (round(random.uniform(1, 50), 2), round(random.uniform(1, 50), 2))
+    im_stack = []
+    shift_stack = []
+    for n in range(n_frames-1):
+        shifts = [round(random.uniform(1, 50), 2), round(random.uniform(1, 50), 2)]
+        shift_stack.append(shifts)
 
-    assert aligned_frames.shape[0] == test_stack[:,0].shape[0]
-    assert calc_shifts == test_stack[:,1]
+        shifted_frames = shift(image, shifts)
+        im_stack.append(shifted_frames)
+    # aligned_frames, calc_shifts = register_frames_stack(frames=test_stack[:,0])
+    aligned_frames, calc_shifts = register_frames_stack(np.asarray(im_stack))
+
+    return aligned_frames, calc_shifts, shift_stack, shifts, np.asarray(im_stack)
+
+    # assert calc_shifts == shift_stack
+    # assert calc_shifts == test_stack[:,1]
 
 
-# if __name__ == "__main__":
-#     test_stack()
+if __name__ == "__main__":
+    test_stack()
